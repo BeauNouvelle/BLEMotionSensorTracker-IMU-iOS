24362edcaac341236c9fc374873f5a33c22eacd5
Beau Nouvelle <beau@getswifty.dev>
2022-03-07 12:45:29 +1100

    Initial Commit

---
+++ MovementTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovementTracker
+//
+//  Created by Beau Nouvelle on 7/3/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovementTracker/MovementTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovementTrackerApp.swift
+//  MovementTracker
+//
+//  Created by Beau Nouvelle on 7/3/2022.
+//
+
+import SwiftUI
+
+@main
+struct MovementTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
